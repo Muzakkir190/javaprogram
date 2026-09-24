426b4ef8cc957c442e3e76965f6d2061ffc236ed
Muzakkir <ms.bagwan190@gmail.com>
2026-09-24 22:36:30 +0530

    first commit for javaprogram

---
+++ src/javaprogram/Main.java
@@ -0,0 +1,15 @@
+package javaprogram;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Muzakkir");
+		System.out.println("Hello i am bagwan");
+		System.out.println("Hello khan abdul  gaffar khan");
+
+
+	}
+
+}
